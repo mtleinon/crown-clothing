e7e93d0970ef5a3072cacb520c66419d65cc1372
mtleinon <mika.t.leinonen@gmail.com>
2019-11-10 11:17:11 +0200

    Created first version of homepage.

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Directory from './Directory';
+import './Homepage.scss';
+
+export default function Homepage() {
+
+  return (
+    <div className="homepage">
+      <Directory/>
+    </div>
+  )
+}
