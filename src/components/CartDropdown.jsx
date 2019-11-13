4e59da30bce04559f9e1c6af561500556d22db42
mtleinon <mika.t.leinonen@gmail.com>
2019-11-13 17:03:46 +0200

    Added cart items list to cart drop down.

--- src/components/CartDropdown.jsx
+++ src/components/CartDropdown.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import {useSelector} from 'react-redux';
+
 import './CartDropdown.scss';
 import CustomButton from './CustomButton';
+import CartItem from './CartItem';
+
 export default function CartDropdown() {
+  const cartItems = useSelector(state => state.cart.cartItems);
+
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
+        {cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem}/>
+        ))}
       </div>
-        <CustomButton  >GO TO CHECKOUT</CustomButton>
+      <CustomButton  >GO TO CHECKOUT</CustomButton>
     </div>
     )
 }
